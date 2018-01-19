92a98a17a953a7598f11be2d9c27031ac382ca04
Eduardo Chuquilin <edu240491@gmail.com>
2018-01-19 13:46:56 -0500

    adding locale

---
+++ app/src/main/java/com/chukslabs/trical/data/MyCoursesConstants.java
@@ -0,0 +1,24 @@
+package com.chukslabs.trical.data;
+
+/**
+ * Created by echuquilin on 27/06/17.
+ */
+public class MyCoursesLang {
+
+    public static final String EXTRA_OBJECT_COURSE = "courseObject";
+    public static final String EXTRA_OBJECT_EXAMS = "examsObject";
+
+    // Snackbars
+    public static final String SNACKBAR_OK = "OK";
+
+    // Dialogs
+    public static final String DIALOG_OK = "OK";
+    public static final String DIALOG_ERROR = "ERROR";
+    public static final String DIALOG_CANCEL = "Cancelar";
+    public static final String DIALOG_REMOVE_TITLE = "Eliminar Curso";
+    public static final String DIALOG_REMOVE_MESSAGE = "¿Seguro/a desea eliminar %s?";
+    public static final String DIALOG_REMOVE_POSITIVE = "Eliminar";
+    public static final String DIALOG_COURSE_DUPLICATED_CODE = "Ya existe un curso con ese código y nombre en ese centro de estudios en el mismo periodo";
+    public static final String DIALOG_COURSE_GRADES_ERROR = "La mayor nota obtenible no puede ser menor que la mínima nota para aprobar";
+
+}
---
+++ app/src/main/java/com/chukslabs/trical/ui/courses/CoursesListener.java
@@ -0,0 +1,8 @@
+package com.chukslabs.trical.ui.courses;
+
+/**
+ * Created by educhuks on 1/5/18.
+ */
+
+public interface CoursesListener {
+}
